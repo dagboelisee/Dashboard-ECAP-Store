1e1af8d50d0f4211a7d76944085836d76041e972
dagboelisee <dagboelisee7@gmail.com>
2026-03-20 16:13:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -551,7 +551,7 @@ app.layout = html.Div(
                             style={
                                 "width": "44%",
                                 "height": "380px",
-                                "marginTop": "-130px"
+                                "marginTop": "-123px"
                             }
                         ),
 
